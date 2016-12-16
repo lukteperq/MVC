a07e7e01b66f14678863d0ef9d7b6d9733716c9d
lukteperq <grimstadoyvind@gmail.com>
2016-12-16 16:19:06 +0100

    First commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once('../app/init.php');
+  $object = new Controller;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MVC framework</title>
+  </head>
+  <body>
+    <?php echo "index"; ?>
+
+  </body>
+</html>
